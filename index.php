d2d1f3b57ae1745b9be58d8bc238ded31e2ece5a
Nishant Vaity <enishant@users.noreply.github.com>
2014-05-03 16:01:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 /**
 Plugin Name: bValidator
-Plugin URI: https://github.com/enishant
+Plugin URI: http://wordpress.org/plugins/bvalidator/
 Description: Includes jQuery and CSS for bValidator
 Tags: bvalidator,jquery,validation.validator
 Version: 1.0
 Author: Nishant Vaity
-Author URI: https://github.com/enishant
+Author URI: http://profiles.wordpress.org/enishant/
 License: GPL2
-Reference : http://bojanmauser.from.hr/bvalidator/
 */
 
 class bValidator
@@ -17,8 +16,9 @@ class bValidator
 	{
 		add_action( 'wp_enqueue_scripts', array($this,'bvalidator_enqueue_scripts'));	
 		add_shortcode('binitform', array($this,'bvalidator_init_form'));
+		register_activation_hook( __FILE__, array($this,'send_email_on_plugin_activate'));
 	}
-
+	
 	function bvalidator_enqueue_scripts() 
 	{
 		wp_enqueue_style( 'bvalidator-css', plugins_url( 'bvalidator.css' , __FILE__ ) );
@@ -40,5 +40,53 @@ class bValidator
 		}
 		return $bvalidator_init;
 	}
+
+	function send_email_on_plugin_activate() 
+	{
+		$plugin_title = "bValidator";
+		$plugin_url = 'http://wordpress.org/plugins/bvalidator/';
+		$plugin_support_url = 'http://wordpress.org/support/plugin/bvalidator';
+		$plugin_author = 'Nishant Vaity';
+		$plugin_author_url = 'https://github.com/enishant';
+		$plugin_author_mail = 'enishant@gmail.com';
+
+		$website_name  = get_option('blogname');
+		$adminemail = get_option('admin_email');
+		$user = get_user_by( 'email', $adminemail );
+
+		$headers = 'From: ' . $website_name . ' <' . $adminemail . '>' . "\r\n";
+		$subject = "Thank you for installing " . $plugin_title . "!\n";
+		if($user->first_name)
+		{
+			$message = "Dear " . $user->first_name . ",\n\n";
+		}
+		else
+		{
+			$message = "Dear Administrator,\n\n";
+		}
+		$message.= "Thank your for installing " . $plugin_title . " plugin.\n";
+		$message.= "Visit this plugin's site at " . $plugin_url . " \n\n";
+		$message.= "Please write your queries and suggestions at developers support \n" . $plugin_support_url ."\n";
+		$message.= "All the best !\n\n";
+		$message.= "Thanks & Regards,\n";
+		$message.= $plugin_author . "\n";
+		$message.= $plugin_author_url ;
+		wp_mail( $adminemail, $subject, $message,$headers);
+		
+		$subject = $plugin_title . " plugin is installed and activated by website " . get_option('home') ."\n";
+		$message = $plugin_title  . " plugin is installed and activated by website " . get_option('home') ."\n\n";
+		$message.= "Website : " . get_option('home') . "\n";
+		$message.="Email : " . $adminemail . "\n";
+		if($user->first_name)
+		{
+			$message.= "First name : " . $user->first_name . " \n";
+		}
+		if($user->last_name)
+		{
+			$message.= "Last name : " . $user->last_name . "\n";	
+		}
+		wp_mail( $plugin_author_mail , $subject, $message,$headers);
+	}
 }
 new bValidator;
+?>
